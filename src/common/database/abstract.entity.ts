01ab8aec5bb949f6a6f5ddb2c0e0d09097664b2a
Ace De Castro <acetdecastro@gmail.com>
2024-01-27 23:43:06 +0800

    Add Find All Users

--- src/common/database/abstract.entity.ts
+++ src/common/database/abstract.entity.ts
@@ -1,9 +1,12 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql';
 import { Prop, Schema } from '@nestjs/mongoose';
 import { SchemaTypes, Types } from 'mongoose';
 
 // Represents base Mongo Document
 @Schema()
-export class AbstractDocument {
+@ObjectType({ isAbstract: true })
+export class AbstractEntity {
   @Prop({ type: SchemaTypes.ObjectId })
+  @Field(() => ID)
   _id: Types.ObjectId;
 }
