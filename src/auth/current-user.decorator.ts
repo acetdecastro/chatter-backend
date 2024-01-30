e85a11455188f69c6fac080102cd9f16c4e638ec
Ace De Castro <acetdecastro@gmail.com>
2024-01-30 20:13:11 +0800

    Chore: Add GqlAuthGuard

--- src/auth/current-user.decorator.ts
+++ src/auth/current-user.decorator.ts
@@ -1,9 +1,14 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+import { GqlContextType, GqlExecutionContext } from '@nestjs/graphql';
 import { User } from 'src/users/entities/user.entity';
 
 const getUserFromContext = (context: ExecutionContext): User => {
-  // Switch context to HTTP and extract in-flight user
-  return context.switchToHttp().getRequest().user;
+  if (context.getType() === 'http') {
+    // Switch context to HTTP and extract in-flight user
+    return context.switchToHttp().getRequest().user;
+  } else if (context.getType<GqlContextType>() === 'graphql') {
+    return GqlExecutionContext.create(context).getContext().req.user;
+  }
 };
 
 export const CurrentUser = createParamDecorator(
